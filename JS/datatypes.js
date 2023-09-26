b51236b6cf9f5f13f81ca738951b32e6b18035c1
Tawsief sofi <70218402+tawsiefsofi@users.noreply.github.com>
2023-09-26 16:24:17 +0000

    datatyes

---
+++ JS/datatypes.js
@@ -0,0 +1,17 @@
+let name ="furqan" //String
+let age = 40 // number
+let passExam = true // bool
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof passExam);
+ const x = null;
+ let a;
+console.log(typeof x);
+console.log(typeof a);
+
+let s = Symbol("tawsief")
+let f = Symbol("tawsief")
+console.log(typeof s);
+
+
